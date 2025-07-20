060efc8cb4e75ef5bd29e2647e67a0ff2b68c11d
yasirk1818 <yasirk1818@gmail.com>
2025-07-20 14:47:52 -0700

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,59 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+// @desc    Register a new user
+// @route   POST /api/auth/signup
+// @access  Public
+const signupUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'Please enter all fields' });
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: 'User with this email already exists' });
+  }
+
+  const user = await User.create({ username, email, password });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({ message: 'Invalid user data' });
+  }
+};
+
+// @desc    Auth user & get token
+// @route   POST /api/auth/login
+// @access  Public
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    if (user.isBlocked) {
+        return res.status(403).json({ message: 'Your account has been blocked' });
+    }
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).json({ message: 'Invalid email or password' });
+  }
+};
+
+module.exports = { signupUser, loginUser };
